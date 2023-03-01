9dfce027342209c674932efa96efaa3420c8681b
Clarence Bermudez <csbermudez04@gmail.com>
2023-03-01 11:39:19 -0800

    hello

--- grade.sh
+++ grade.sh
@@ -1,5 +1,24 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+RUNPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+echo 'ListExample.java found'
+else 
+echo 'ListExamples.java not found'
+exit
+fi
+cp student-submission/ListExamples.java ./
+javac -cp $CPATH *.java 
+if [[ $? -ne 0 ]]
+then 
+echo 'Compile error'
+else 
+echo 'Compile success'
+fi
+java -cp $RUNPATH TestListExamples > results.txt
+cat results.txt
